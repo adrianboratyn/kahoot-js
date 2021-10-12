bd63f5314b9d073af63dc3c19e9856f804041938
Adrian Boratyn <adrian.boratyn@icloud.com>
2021-10-12 15:22:05 +0200

    Add basic express server

---
+++ server/server.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+mongoose.connect(process.env.DATABASE_URL);
+
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to database"));
+
+app.use(express.json());
+
+app.listen(process.env.PORT, () =>
+  console.log(`Server started on port ${process.env.PORT}`)
+);
