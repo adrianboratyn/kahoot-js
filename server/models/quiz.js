711215287b625f59402b3b85baaf35b30009c752
Adrian Boratyn <adrian.boratyn@icloud.com>
2021-10-14 14:48:25 +0200

    Update quiz model

--- server/models/quiz.js
+++ server/models/quiz.js
@@ -2,27 +2,44 @@ const mongoose = require("mongoose");
 
 const quizSchema = new mongoose.Schema({
   name: { type: String, required: true },
+  description: {type: String},
   creatorId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
-  },
-  questionType: {
-    type: String,
-    enum: ["True/False", "Multiple-choice", "Open"],
-    required: true,
   },
   pointType: {
     type: String,
     enum: ["Standard", "Double", "BasedOnTime"],
     required: true,
   },
-  points: { type: Number },
-  answerTime: { type: Number },
-  numberOfQuestions: { type: Number },
-  questionList: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Question",
+  pointsPerQuestion: { 
+    type: Number,
+    min: 1,
   },
+  answerTime: { 
+    type: Number,
+    min: 5,
+    max: 15,
+  },
+  numberOfQuestions: { 
+    type: Number,
+    default: 0, 
+  },
+  questionList: [
+    {
+      questionType: {
+        type: String,
+        enum: ["True/False", "Multiple-choice", "Open"],
+        required: true,
+      },
+      question: {
+        type: String,
+        required: true,
+      },
+      answerList: [String],
+      correctAnswer: { type: Number },
+    },
+  ],
 });
 
 module.exports = mongoose.model("Quiz", quizSchema);
