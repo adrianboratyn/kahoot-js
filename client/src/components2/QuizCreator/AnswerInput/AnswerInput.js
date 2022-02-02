8691e48e218d5ae15172051a5a03ed8887ade315
Adrian Boratyn <adrian.boratyn@icloud.com>
2022-02-02 13:48:59 +0100

    Update AnswerInput

--- client/src/components2/QuizCreator/AnswerInput/AnswerInput.js
+++ client/src/components2/QuizCreator/AnswerInput/AnswerInput.js
@@ -1,27 +1,20 @@
-import React, { useState } from "react"
+import React from "react"
 import styles from "./answerInput.module.css"
-import img6 from "../../../assets/img6.svg"
+import answerCheck from "../../../assets/answerCheck.svg"
 
 
 function AnswerInput({ value, onChange, onClick, isAnswerCorrect, svg }) {
-  // const [isAnswerCorrect, setIsAnswerCorrect] = useState(false)
 
-  // const setCorrectAnswer = () => {
-  //   setIsAnswerCorrect((prevIsAnswerCorrect) => !prevIsAnswerCorrect)
-  //   answer = isAnswerCorrect
-  // }
   return (
     <>
       <img src={svg} alt="" />
       <input type="text" value={value} onChange={onChange} name="a" />
-      {/* <!-- checkbox która odpowiedź poprawna --> */}
       <div onClick={onClick} className={styles["answer-check"]}>
         <img
           style={{ visibility: isAnswerCorrect ? "visible" : "hidden" }}
-          src={img6}
+          src={answerCheck}
           alt=""
         />
-        {/* <input name="a" type="checkbox" /> */}
       </div>
     </>
   )
