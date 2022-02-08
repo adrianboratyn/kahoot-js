263f0779ed3deea31c1c78d629231fdb49a654fb
Adrian Boratyn <adrian.boratyn@icloud.com>
2022-02-08 09:40:25 +0100

    Increase maximum request size

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to database"));
 
-app.use(express.json());
+app.use(express.json({limit: '5mb'}));
 app.use(cors());
 app.use(authenticateToken);
 //app.use(regenerateAccessToken);
