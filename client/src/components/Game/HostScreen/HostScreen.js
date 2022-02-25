52171367a8570e8a701a44567ef087fea6ac43ed
Adrian Boratyn <adrian.boratyn@icloud.com>
2022-02-25 19:48:27 +0100

    Updates in host page and player page

--- client/src/components/Game/HostScreen/HostScreen.js
+++ client/src/components/Game/HostScreen/HostScreen.js
@@ -4,18 +4,37 @@ import WaitingRoom from "../WaitingRoom/WaitingRoom"
 import { useDispatch, useSelector } from "react-redux"
 import { getGame } from "../../../actions/game"
 import { getQuiz } from "../../../actions/quiz"
-import { io } from "socket.io-client"
-
-const socket = io("http://localhost:3001")
+import styles from "./hostScreen.module.css"
 
 function HostScreen() {
+  const socket = useSelector((state) => state.socket.socket)
   const [isGameStarted, setIsGameStarted] = useState(false)
+  const [isQuestionDisplayed, setIsQuestionDisplayed] = useState(false)
+  const [isCountdownStarted, setIsCountdownStarted] = useState(false)
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0)
+  const [timer, setTimer] = useState(0)
+  const [gameData, setGameData] = useState({
+    isLive: "",
+    playerResultList: [],
+  })
+  const [questionData, setQuestionData] = useState({
+    questionType: "Quiz",
+    pointType: "Standard",
+    answerTime: 5,
+    backgroundImage: "",
+    question: "",
+    answerList: [
+      { name: "a", body: "", isCorrect: false },
+      { name: "b", body: "", isCorrect: false },
+      { name: "c", body: "", isCorrect: false },
+      { name: "d", body: "", isCorrect: false },
+    ],
+    questionIndex: 1,
+  })
   const dispatch = useDispatch()
   const { id } = useParams()
   const { game } = useSelector((state) => state.games)
   const { quiz } = useSelector((state) => state.quiz)
-  console.log(game)
-  console.log(quiz)
 
   useEffect(() => {
     dispatch(getGame(id))
@@ -26,22 +45,110 @@ function HostScreen() {
       dispatch(getQuiz(game.quizId))
     }
   }, [dispatch, game])
- 
+
   useEffect(() => {
-    socket.on("host-start-game", ()=>{
-      setIsGameStarted(true)
+    socket.on("question-countdown", () => {
+      console.log("hh")
+      // startCountdown()
+    })
+
+    socket.on("send-question-to-host", (question, time) => {
+      setQuestionData(question)
+      setIsQuestionDisplayed((prevstate) => !prevstate)
+      setCurrentQuestionIndex((prevstate) => prevstate + 1)
+      setTimer(time)
+      // startCountdown()
     })
   }, [])
 
+  // useEffect(() => {
+  //   socket.on("host-start-game", () => {
+  //     socket.emit("question-preview", () => {
+  //       //startCountdown()
+  //     })
+  //   })
+  // }, [socket])
+
+  useEffect(() => {
+    setTimer(5)
+  }, [])
+
+  const startGame = () => {
+    socket.emit("start-game", quiz)
+    socket.emit("question-preview", () => {
+      startPreviewCountdown(5)
+    })
+    setIsGameStarted(prevstate => !prevstate)
+    setIsCountdownStarted(prevstate => !prevstate)
+  }
+
+  // const showTimer = () => {
+  //   console.log("ss")
+  //   socket.emit("show-timer")
+  //   startCountdown()
+  //   // setIsCountdownStarted((prevstate) => !prevstate)
+  // }
+
+  const startPreviewCountdown = (seconds) => {
+    let time = seconds
+    let interval = setInterval(() => {
+      setTimer(time)
+      if (time === 0) {
+        clearInterval(interval)
+        let question = quiz.questionList[currentQuestionIndex]
+        socket.emit("send-question-to-socket", question)
+        setIsCountdownStarted(prevstate => !prevstate)
+        displayQuestion(currentQuestionIndex)
+        // socket.emit("start-question-timer", () => {
+        //   startQuestionCountdown(quiz.questionList[currentQuestionIndex].answerTime)
+        // })
+      }
+      time--
+    }, 1000)
+  }
+
+  const displayQuestion = (index) => {
+    setQuestionData( quiz.questionList[index]
+      // questionType: quiz.questionList[index].questionType,
+      // pointType: quiz.questionList[index].pointType,
+      // answerTime: quiz.questionList[index].answerTime,
+      // backgroundImage: quiz.questionList[index].backgroundImage,
+      // question: quiz.questionList[index].question,
+      // answerList: [
+      //   {
+      //     name: "a",
+      //     body: quiz.questionList[index].answerList[0].body,
+      //     isCorrect: quiz.questionList[index].answerList[0].isCorrect,
+      //   },
+      //   {
+      //     body: quiz.questionList[index].answerList[1].body,
+      //     isCorrect: quiz.questionList[index].answerList[1].isCorrect,
+      //   }
+      // ],
+      // questionIndex: currentQuestionIndex + 1,
+    )
+  }
+
   return (
-    <div>
-      {!isGameStarted ? (
-        <WaitingRoom pin={game?.pin} />
-      ) : (
-        <div>
-          <h1>Game</h1>
+    <div className={styles.page}>
+      {!isGameStarted && (
+        <div className={styles.lobby}>
+          <WaitingRoom pin={game?.pin} socket={socket} />
+          <button onClick={startGame}>Start a game</button>
         </div>
       )}
+
+      {isGameStarted && isCountdownStarted && (
+        <div className={styles["question-preview"]}>
+          <h1>{timer}</h1>
+        </div>
+      )}
+      {isGameStarted && !isCountdownStarted && (
+        <div className={styles["question-preview"]}>
+            <h3>{questionData.question}</h3>
+        </div>
+      )}
+    
     </div>
   )
 }
--- client/src/components/Game/PlayerScreen/PlayerScreen.js
+++ client/src/components/Game/PlayerScreen/PlayerScreen.js
@@ -1,18 +1,93 @@
 import React from "react"
 import { useState } from "react"
-import WaitingRoom from "../WaitingRoom/WaitingRoom"
+import { useDispatch, useSelector } from "react-redux"
+import { addAnswer } from "../../../actions/playerResult"
+import { useEffect } from "react"
+import styles from "./playerScreen.module.css"
 
 function PlayerScreen() {
-  const [gameStarted, setGameStarted] = useState(false)
-  return (
-    <div>
-      {!gameStarted && 
-      <WaitingRoom />
+  const socket = useSelector((state) => state.socket.socket)
+  // getPlayerResult i zapisać go w zmiennej
+  // później przy kazdym wysyłaniu odpowiedzi robić addAnswer
+  const { playerResult } = useSelector((state) => state.playerResults)
+  // const [gameStarted, setGameStarted] = useState(false)
+  const [isGameStarted, setIsGameStarted] = useState(true)
+  const [isQuestionDisplayed, setIsQuestionDisplayed] = useState(false)
+  const [isCountdownStarted, setIsCountdownStarted] = useState(true)
+  const [timer, setTimer] = useState(0)
+  const [playerResultData, setPlayerResultData] = useState({
+    playerId: "",
+    gameId: "",
+    quizId: "",
+    score: 0,
+    answers: [
+      {
+        questionIndex: 0,
+        answered: false,
+        answers: [],
+        correctAnswers: [],
+        time: 0,
+        points: 0,
+      },
+    ],
+  })
+  const [answer, setAnswer] = useState({
+    questionIndex: 0,
+    answered: false,
+    answers: [],
+    time: 0,
+    points: 0,
+  })
+
+  useEffect(() => {
+    setTimer(5)
+  }, [])
+
+  useEffect(() => {
+    socket.on("host-start-preview", () => {
+      startPreviewCountdown(5)
+    })
+  }, [])
+
+  const startPreviewCountdown = (seconds) => {
+    let time = seconds
+    let interval = setInterval(() => {
+      setTimer(time)
+      if (time === 0) {
+        clearInterval(interval)
+        // let question = quiz.questionList[currentQuestionIndex]
+        // socket.emit("send-question-to-socket", question)
+        setIsCountdownStarted((prevstate) => !prevstate)
+        // displayQuestion(currentQuestionIndex)
+        // socket.emit("start-question-timer", () => {
+        //   startQuestionCountdown(quiz.questionList[currentQuestionIndex].answerTime)
+        // })
       }
-      <h1>Player</h1>
+      time--
+    }, 1000)
+  }
+  //moze jeszcze answerCount zeby czas zatrzymac dopiero jak sie odpowie na wszystkie mozliwe
+  //i jeszcze info ze kilka poprawnych odpowiedzi
+
+  // po kliknięciu w odp disptach(addAnswer)
+  // po odliczeniu czasu tez dispatch ale z answer = false
+  // wtedy aktualizować state answer i wysłać go w sockecie
+  // zaktualizować tez playerResultData
+
+  return (
+    <div className={styles.page}>
+      {isGameStarted && isCountdownStarted && (
+        <div className={styles["question-preview"]}>
+          <h1>{timer}</h1>
+        </div>
+      )}
+      {isGameStarted && !isCountdownStarted && (
+        <div className={styles["question-preview"]}>
+          <h3>Tu będzie pytanie</h3>
+        </div>
+      )}
     </div>
   )
-  
 }
 
 export default PlayerScreen
