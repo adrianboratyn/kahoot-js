b58aacdab0799d60c90664bbe93538f0961b21e3
Adrian Boratyn <adrian.boratyn@icloud.com>
2022-03-01 10:28:03 +0100

    Update socket server

--- server/socketServer.js
+++ server/socketServer.js
@@ -1,60 +1,78 @@
+const { instrument } = require("@socket.io/admin-ui")
+
 const io = require("socket.io")(3001, {
   cors: {
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://admin.socket.io/#/sockets"],
   },
 })
-
-const {
-  createGame,
-  getGames,
-  getGame,
-  updateGame,
-  deleteGame,
-} = require("./controllers/game")
 
 let game
 let players = []
 
-const addPlayer = (user) => {
-  !players.some((player) => player._id === user._id) &&
-    players.push(user)
+
+const addPlayer = (userName, socketId) => {
+  !players.some((player) => player.socketId === socketId) &&
+    players.push({ userName, socketId })
 }
 
-const getPlayer = (playerId) => {
-  return players.find((player) => player._id === playerId)
+const getPlayer = (socketId) => {
+  return players.find((player) => player.socketId === socketId)
 }
 
 io.on("connection", (socket) => {
-  console.log(socket.id)
+  socket.on("disconnect", (reason) => {
+    console.log("Socket " + socket.id + " was disconnected")
+    console.log(reason)
+  })
   socket.on("init-game", (newGame) => {
     game = JSON.parse(JSON.stringify(newGame))
-    console.log("Nowa gra 1")
-    console.log(game) 
-    socket.join(game.pin)
+    hostId = socket.id
+    console.log(
+      "Host with id " + socket.id + " started game and joined room: " + game.pin
+    )
   })
+
   socket.on("add-player", (user, socketId, pin, cb) => {
-    console.log(`Student with id: ${user._id} joined a game ${pin}`)
-    console.log(game.pin)
-    // console.log(user);
-    // console.log(socketId);
-    if (game.pin === pin) { 
-      addPlayer(user)
-      cb("correct", game._id) 
-      socket.join(game.pin)  
-      console.log("Socket with id " + socket.id + " joined room")
-      let player = getPlayer(user._id)
-      // console.log(player);  
-      // console.log("Dodany gracz" + player);
-      io.emit("player-added", player) 
-    } else { 
+    if (game.pin === pin) {
+      addPlayer(user.userName, socketId)
+      console.log(game._id)
+      cb("correct", user._id, game._id)
+      socket.join(game.pin)
+      console.log(
+        "Student " +
+          user.userName +
+          " with id " +
+          socket.id + 
+          " joined room " +
+          game.pin
+      )
+      let player = getPlayer(socketId)
+      io.emit("player-added", player)
+    } else {
       cb("wrong", game._id)
     }
   })
-  socket.on("start-game", () => {  
-    console.log(players);
-    console.log(game); 
+
+  socket.on("start-game", (newQuiz) => {
+    quiz = JSON.parse(JSON.stringify(newQuiz))
+    console.log("Move players to game")
+    console.log(game.pin)
     socket.to(game.pin).emit("move-to-game-page", game._id)
-    io.emit("host-start-game")
+  })
+
+  socket.on("question-preview", (cb) => { 
+    cb() 
+    socket.to(game.pin).emit("host-start-preview")
+  }) 
+
+  socket.on("start-question-timer", (time, question, cb) => { 
+    console.log(question);
+    console.log("Send question " + question.questionIndex + " data to players")
+    socket
+      .to(game.pin)
+      .emit("host-start-question-timer", time, question)
+    cb()
   })
 })
- 
+
+instrument(io, { auth: false })
