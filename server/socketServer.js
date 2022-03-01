51ed4455815374cdde23be8386e16132050d844c
Adrian Boratyn <adrian.boratyn@icloud.com>
2022-03-01 21:48:39 +0100

    Update socket server

--- server/socketServer.js
+++ server/socketServer.js
@@ -7,8 +7,8 @@ const io = require("socket.io")(3001, {
 })
 
 let game
+let leaderboard 
 let players = []
-
 
 const addPlayer = (userName, socketId) => {
   !players.some((player) => player.socketId === socketId) &&
@@ -24,8 +24,10 @@ io.on("connection", (socket) => {
     console.log("Socket " + socket.id + " was disconnected")
     console.log(reason)
   })
-  socket.on("init-game", (newGame) => {
+  socket.on("init-game", (newGame, newLeaderboard) => {
     game = JSON.parse(JSON.stringify(newGame))
+    leaderboard = JSON.parse(JSON.stringify(newLeaderboard))
+    socket.join(game.pin) 
     hostId = socket.id
     console.log(
       "Host with id " + socket.id + " started game and joined room: " + game.pin
@@ -35,14 +37,14 @@ io.on("connection", (socket) => {
   socket.on("add-player", (user, socketId, pin, cb) => {
     if (game.pin === pin) {
       addPlayer(user.userName, socketId)
-      console.log(game._id)
+      // console.log(game._id)
       cb("correct", user._id, game._id)
       socket.join(game.pin)
       console.log(
         "Student " +
           user.userName +
           " with id " +
-          socket.id + 
+          socket.id +
           " joined room " +
           game.pin
       )
@@ -55,24 +57,28 @@ io.on("connection", (socket) => {
 
   socket.on("start-game", (newQuiz) => {
     quiz = JSON.parse(JSON.stringify(newQuiz))
-    console.log("Move players to game")
+    console.log("Move players to game") 
     console.log(game.pin)
     socket.to(game.pin).emit("move-to-game-page", game._id)
-  })
-
-  socket.on("question-preview", (cb) => { 
-    cb() 
-    socket.to(game.pin).emit("host-start-preview")
   }) 
 
-  socket.on("start-question-timer", (time, question, cb) => { 
-    console.log(question);
-    console.log("Send question " + question.questionIndex + " data to players")
-    socket
-      .to(game.pin)
-      .emit("host-start-question-timer", time, question)
+  socket.on("question-preview", (cb) => {
     cb()
+    socket.to(game.pin).emit("host-start-preview")
+  })
+
+  socket.on("start-question-timer", (time, question, cb) => {
+    // console.log(question)
+    console.log("Send question " + question.questionIndex + " data to players")
+    socket.to(game.pin).emit("host-start-question-timer", time, question)
+    cb()
+  }) 
+
+  socket.on("send-answer-to-host", (data, score) => {
+    let player = getPlayer(socket.id)
+    socket.to(game.pin).emit("get-answer-from-player", data, leaderboard._id, score, player)
   })
 })
 
 instrument(io, { auth: false })
+ 
